4b7b1df19e8c842f8c611a8440edfd1032baaabd
MohamedHaro0on <67293364+MohamedHaro0on@users.noreply.github.com>
2021-04-19 02:37:35 +0200

    Deploying the project to gh-pages

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ const App = ()=>{
     axios.get(URL).then((response)=>{
       setData (response.data);
       setLoading(false);
+      let i = 3; let j = 4; let k = 1; for (i = 0; i < 3; i++) { k += j; j = j - 1; }
+      console.log("THe Value or k " , k )
+      
     })
   }
 
